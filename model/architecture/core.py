bee087020efaaa4e33f183587e84fa587eedd08a
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-04-22 00:17:32 +0900

    ADD: Factorization Machine

--- model/architecture/core.py
+++ model/architecture/core.py
@@ -3,13 +3,13 @@ from typing import List
 import torch
 import torch.nn as nn
 
-from model.inputs import Feature, create_embedding_matrix
 from model.architecture._model_object import _ModelObject
+from model.inputs import Feature, SparseFeature, create_embedding_matrix
 
 
 class Linear(_ModelObject):
-    def __init__(self, linear_features: List[Feature], device: str = "cpu"):
-        super().__init__(linear_features)
+    def __init__(self, features: List[Feature], device: str = "cpu"):
+        super().__init__(features)
 
         self.device = device
 
@@ -40,43 +40,19 @@ class Linear(_ModelObject):
         return logit
 
 
-if __name__ == "__main__":
-    import numpy as np
-    import pandas as pd
+class FactorizationMachine(_ModelObject):
+    def __init__(self, sparse_features: List[SparseFeature], device: str = "cpu"):
+        super().__init__(sparse_features)
 
-    from model.inputs import SparseFeature, DenseFeature
+        self.device = device
 
-    DATASET_PATH = "/Users/youngho/Documents/GitHub/click-like/data/prep_test.csv"
+        self.embedding_dict = create_embedding_matrix(self.sparse_features, linear=False).to(self.device)
 
-    TARGET = ["label"]
-    SPARSE_FEATURES = [f"C{index}" for index in range(1, 27)]
-    DENSE_FEATURES = [f"I{index}" for index in range(1, 14)]
+    def forward(self, x: torch.Tensor):
+        embedding_values = self.get_sparse_embedding_values(x, self.embedding_dict)
+        embedding_values = torch.cat(embedding_values, dim=1)
 
-    data_frame = pd.read_csv(DATASET_PATH)
+        square_of_sum = torch.pow(torch.sum(embedding_values, dim=1), exponent=2)
+        sum_of_square = torch.sum(torch.pow(embedding_values, exponent=2), dim=1)
 
-    sparse_feature_columns = [
-        SparseFeature(feature, vocabulary_size=data_frame[feature].max() + 1, embedding_dim=4)
-        for feature in SPARSE_FEATURES
-    ]
-    dense_feature_columns = [
-        DenseFeature(feature, dim=1, )
-        for feature in DENSE_FEATURES
-    ]
-    features = sparse_feature_columns + dense_feature_columns
-
-    train_inputs = {
-        feature.name: data_frame[feature.name]
-        for feature in features
-    }
-
-    model = Linear(features)
-
-    x = [train_inputs[feature_name] for feature_name in model.feature_offset.keys()]
-
-    for index in range(0, len(x)):
-        if len(x[index].shape) == 1:
-            x[index] = np.expand_dims(x[index], axis=1)
-
-    x = torch.from_numpy(np.concatenate(x, axis=-1)).float()
-    print(model(x))
-
+        return torch.sum(square_of_sum - sum_of_square, dim=1) / 2
